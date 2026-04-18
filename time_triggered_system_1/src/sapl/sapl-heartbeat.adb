02a14cc0412ee133b58e2afa1ddcee946dcb5009
Vrumugun <wisniewski1@gmail.com>
2026-04-18 15:22:07 +0200

    test reading input

--- time_triggered_system_1/src/sapl/sapl-heartbeat.adb
+++ time_triggered_system_1/src/sapl/sapl-heartbeat.adb
@@ -10,14 +10,28 @@ package body SAPL.Heartbeat is
    Counter : HAL.UInt32 := 0;
    Counter_Duplicate : HAL.UInt32 := (2 ** 32 - 1);
 
+   State : Boolean := False;
+
    procedure Initialize is
    begin
       Pico.LED.Configure (RP.GPIO.Output);
       Pico.GP18.Configure (RP.GPIO.Output);
+      
+      Pico.GP4.Configure (RP.GPIO.Output);
+      Pico.GP5.Configure (RP.GPIO.Input);
+
    end Initialize;
 
    procedure Update is
    begin
+      Pico.GP4.Set;
+      if Pico.GP5.Get = True and State = False then
+         State := True;
+         COM.Debug.Put_Tx_String (State'Image & Character'Val (13) & Character'Val (10));
+      elsif Pico.GP5.Get = False and State = True then
+         State := False;
+         COM.Debug.Put_Tx_String (State'Image & Character'Val (13) & Character'Val (10));
+      end if;
       if SAPL.Verify_Duplicate_Variable (Counter, Counter_Duplicate) then
          Pico.LED.Toggle;
          Pico.GP18.Toggle;
